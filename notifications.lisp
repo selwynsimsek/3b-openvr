7700bcd6dc7f6ce4c43d59e64cf097394c1404dc
Selwyn Simsek <sgs16@ic.ac.uk>
2019-11-01 11:41:46 +0000

    Sketch out Notifications API

--- notifications.lisp
+++ notifications.lisp
@@ -1,1 +1,18 @@
-; version = IVR_Notifications_002
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; High-level bindings to the IVRNotifications API.
+
+;;; version = IVR_Notifications_002
+
+;; according to openvr.h this interface is not implemented yet anyway...
+
+(in-package 3b-openvr)
+
+(defclass notification ()
+  ((id :initarg :id :accessor id)
+   (removed-p :initarg :removed-p :accessor removed-p)))
+
+(defun create-notification (overlay user-value type text style image &key (notifications *notifications*))
+  (error "implement me"))
+
+(defun remove-notification (notification &key (notifications *notifications*))
+  (%remove-notification (table notifications) (id notification)))
